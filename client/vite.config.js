c606af738358e1c30a3661145d57e69ce0dd3785
Mahaboob <bablusd750@gmail.com>
2025-07-16 12:27:36 +0530

    updated server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:6969',
+        target: 'https://restaurant-app-163i.onrender.com',
         changeOrigin: true,
         secure: false
       }
